6468a49b8e2eb29d4b74afe4fee21741f2c8d7a3
Marc <marc@Marcs-MacBook-Pro.local>
2013-10-21 19:31:29 +0100

    Added functions to swap endianness

---
+++ libraries/Endian/Endian.cpp
@@ -0,0 +1,31 @@
+#include <Arduino.h>
+
+#include "Endian.h"
+
+void Endian::swap16(uint8_t* data)
+{
+  uint8_t swap;
+  swap = *data;
+  *data = *(data+1);
+  *(data+1) = swap;
+}
+
+void Endian::swap32(uint8_t* data)
+{
+  uint8_t swap;
+  swap = *data;
+  *data = *(data+3);
+  *(data+3) = swap;
+  swap = *(data+1);
+  *(data+1) = *(data+2);
+  *(data+2) = swap;
+}
+
+void Endian::multiSwap16(uint8_t* data, int count)
+{
+  for(int i=0; i<count; i++)
+  {
+    swap16(data);
+    data += 2;
+  }
+}
---
+++ libraries/Endian/Endian.h
@@ -0,0 +1,35 @@
+#ifndef ENDIAN_H
+#define ENDIAN_H
+
+#include <Arduino.h>
+
+/*! This class provides functions to swap endiannes. */
+class Endian
+{
+
+  public:
+
+    /*!
+     * This function swaps the endianness of a 16-bits value.
+     *
+     * @param data Pointer to a 16-bits value
+     */
+    static void swap16(uint8_t* data);
+
+    /*!
+     * This function swaps the endianness of a 32-bits value.
+     *
+     * @param data Pointer to a 32-bits value
+     */
+    static void swap32(uint8_t* data);
+
+    /*!
+     * This function swaps the endianness of multiple 16-bits values.
+     *
+     * @param Pointer to the first 16-bits value
+     * @param count Number of 16-bits values to process
+     */
+    static void multiSwap16(uint8_t* data, int count);
+};
+
+#endif // ENDIAN_H
