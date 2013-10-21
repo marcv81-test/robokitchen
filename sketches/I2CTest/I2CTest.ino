9ed9d4e792d77aa62858570bf557dabb549d7868
Marc <marc@Marcs-MacBook-Pro.local>
2013-10-21 19:08:04 +0100

    Added I2C test sketch

---
+++ sketches/I2CTest/I2CTest.ino
@@ -0,0 +1,20 @@
+#include <I2C.h>
+
+#define MPU6050_ADDRESS 0x68
+#define MPU6050_WHO_AM_I 0x75
+
+uint8_t b;
+
+void setup()
+{
+  I2c.begin();
+  Serial.begin(9600);
+  Serial.println("---");
+}
+
+void loop()
+{
+  I2c.read(MPU6050_ADDRESS, MPU6050_WHO_AM_I, 1, &b); // read the MPU-6050 WHO_AM_I register
+  Serial.println(b, 16); // print the result
+  delay(1000);
+}
