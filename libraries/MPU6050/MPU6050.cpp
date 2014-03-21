38a3554b2875375a8c083a5be7cb6f30efa31421
Marc <marc@Marcs-MacBook-Pro.local>
2014-03-21 00:04:49 +0000

    Added MPU6050 digital low pass filter configuration

--- libraries/MPU6050/MPU6050.cpp
+++ libraries/MPU6050/MPU6050.cpp
@@ -10,16 +10,16 @@
 
 void MPU6050::init()
 {
-  uint8_t i2c_error;
+  uint8_t i2c_error, data;
 
   // reset device
-  uint8_t reset = 0b10000000; // bit 7: reset
+  data = 0b10000000; // bit 7: reset
   #ifdef MPU6050_DEBUG
     Serial.println("MPU6050: Resetting");
   #endif
   do
   {
-    i2c_error = I2C::write(MPU6050_ADDRESS, MPU6050_CONFIG, &reset, 1);
+    i2c_error = I2C::write(MPU6050_ADDRESS, MPU6050_CONFIG, &data, 1);
     #ifdef MPU6050_DEBUG
       Serial.print("MPU6050: Status = ");
       Serial.println(i2c_error);
@@ -29,13 +29,13 @@ void MPU6050::init()
   delay(50); // give device time to reset
 
   // disable sleep mode
-  uint8_t wakeup = 0b00000000; // bit 6: sleep mode
+  data = 0b00000000; // bit 6: sleep mode
   #ifdef MPU6050_DEBUG
     Serial.println("MPU6050: Waking up");
   #endif
   do
   {
-    i2c_error = I2C::write(MPU6050_ADDRESS, MPU6050_CONFIG, &wakeup, 1);
+    i2c_error = I2C::write(MPU6050_ADDRESS, MPU6050_CONFIG, &data, 1);
     #ifdef MPU6050_DEBUG
       Serial.print("MPU6050: Status = ");
       Serial.println(i2c_error);
@@ -44,19 +44,37 @@ void MPU6050::init()
   while (i2c_error != 0);
   delay(50); // give device time to wake up
 
-  uint8_t scale = 0b00011000; // bit 4-3: scale
+  // set gyroscope scale
+  data = 0b00011000; // bit 4-3: scale
   #ifdef MPU6050_DEBUG
     Serial.println("MPU6050: Setting gyro scale");
   #endif
   do
   {
-    i2c_error = I2C::write(MPU6050_ADDRESS, MPU6050_SCALE, &scale, 1);
+    i2c_error = I2C::write(MPU6050_ADDRESS, MPU6050_SCALE, &data, 1);
     #ifdef MPU6050_DEBUG
       Serial.print("MPU6050: Status = ");
       Serial.println(i2c_error);
     #endif
   }
   while (i2c_error != 0);
+
+  // set digital low-pass filter
+  #ifdef MPU6050_DLPF_SETTING
+    data = 0b00000111 & MPU6050_DLPF_SETTING; // bit 2-0: DLPF setting
+    #ifdef MPU6050_DEBUG
+      Serial.println("MPU6050: Setting DLPF");
+    #endif
+    do
+    {
+      i2c_error = I2C::write(MPU6050_ADDRESS, MPU6050_DLPF, &data, 1);
+      #ifdef MPU6050_DEBUG
+        Serial.print("MPU6050: Status = ");
+        Serial.println(i2c_error);
+      #endif
+    }
+    while (i2c_error != 0);
+  #endif
 }
 
 uint8_t MPU6050::refresh()
--- libraries/MPU6050/MPU6050.h
+++ libraries/MPU6050/MPU6050.h
@@ -8,6 +8,7 @@
 #define MPU6050_ADDRESS 0x68
 
 // Registers
+#define MPU6050_DLPF 0x1A // 1 byte (bit 4-3: ext sync, bit 2-0: DLPF)
 #define MPU6050_SCALE 0x1B // 1 byte (bit 4-3: scale)
 #define MPU6050_CONFIG 0x6B // 1 byte (bit 7: reset, bit 6: sleep mode)
 #define MPU6050_DATA 0x3B // 3x2 bytes (gyro) + 1x2 bytes (temp) + 3x2 bytes (accel)
